e7026d6192208aae7969a612803a750df20dcc51
zkx <z18161825600@163.com>
2020-03-08 14:17:35 +0800

    commit 1

---
+++ src/main/java/com/example/travel/mapper/ScenicSpotMapper.java
@@ -0,0 +1,8 @@
+package com.example.travel.mapper;
+
+import com.example.travel.pojo.ScenicSpot;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScenicSpotMapper extends CommonMapper<ScenicSpot> {
+}
